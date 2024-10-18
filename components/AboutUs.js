5a2a1fafa6659766c91a68e4e0ce27faa4a8a3cf
Gino Stoian <ginostoian@gmail.com>
2024-10-18 17:50:23 +0300

    replace photos and rext

--- components/AboutUs.js
+++ components/AboutUs.js
@@ -7,23 +7,31 @@ const AboutUs = () => {
       <div className="max-w-[95%] mx-auto relative flex flex-col-reverse py-16 lg:py-0 lg:flex-col">
         <div className="w-full max-w-xl px-4 mx-auto md:px-0 lg:px-8 lg:py-20 lg:max-w-screen-xl">
           <div className="mb-0 lg:max-w-lg lg:pr-8 xl:pr-6">
-            <h2 className="mb-5 font-sans text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-none md:text-center">
-              Cine este in spatele
-              <br className="hidden md:block" />
-              SyS Photo & Design
+            <h2 className="mb-5 text-center text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl sm:leading-none lg:text-left">
+              Cine este in spatele 'aparatului'
+              {/* <br className="hidden md:block" /> */}
             </h2>
-            <p className="mb-5 text-base text-gray-700 md:text-lg md:text-center">
-              Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-              accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
-              quae. explicabo.
+            <p className="mb-5 text-center text-base text-gray-700 md:text-lg lg:text-left">
+              Suntem Iulia si Alina, două surori care iubesc
+              fotografia,poveștile frumoase spuse băieților noștri și iubim
+              poveștile vizuale, care se transformă în amintiri.
             </p>
-            <div className="mb-10 text-center md:mb-16 lg:mb-20">
-              <a
-                href="/"
-                className="inline-flex items-center justify-center w-full h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md md:w-auto bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
-              >
-                Learn more
-              </a>
+            <p className="mb-5 text-base text-gray-700 text-center md:text-lg lg:text-left">
+              Am ales să punem împreună pasiunile noastre, pentru ca tu să te
+              bucuri de cele mai frumoase amintiri și să redescoperi fericirea
+              din spatele zâmbetului în fiecare imagine.
+            </p>
+            <p className="mb-5 text-center text-base text-gray-700 md:text-lg lg:text-left">
+              Fiecare poveste este unică în felul ei, iar noi facem tot
+              posibilul să te cunoaștem cât mai bine înainte de eveniment. Așa
+              ne asigurăm că vom reuși să surprindem exact ceea ce tu îți
+              dorești, iar rezultatul final să fie peste așteptări. De fiecare
+              dată!
+            </p>
+            <div className="mb-10 text-center lg:text-left md:mb-16 lg:mb-20">
+              <button className="text-xl btn btn-primary btn-wide rounded-xl">
+                Hai sa vorbim
+              </button>
             </div>
             <div className="flex flex-col items-center">
               <div className="mb-2 text-sm text-gray-600 md:mb-2">
@@ -98,7 +106,7 @@ const AboutUs = () => {
         <div className="rounded-xl inset-y-0 top-0 right-0 w-full max-w-xl px-4 mx-auto mb-6 md:px-0 lg:pl-8 lg:pr-0 lg:mb-0 lg:mx-0 lg:w-1/2 lg:max-w-full lg:absolute xl:px-0">
           <Image
             className="object-cover w-full h-56 rounded-xl shadow-lg lg:shadow-none md:h-96 lg:h-full"
-            src="/images/wedding1.webp"
+            src="/images/team-photo.webp"
             alt=""
             width={500}
             height={500}
