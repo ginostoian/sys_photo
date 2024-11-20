527c78c97baeab122d447ee57e0bacc17117fd0e
Gino Stoian <ginostoian@gmail.com>
2024-11-21 01:20:30 +0200

    add content to fix broken component

--- config.js
+++ config.js
@@ -23,6 +23,20 @@ const config = {
         ],
       },
     },
+    aboutUs: {
+      hero: {
+        title: "Sys Photo & Design, pasiune inainte de toate",
+        subtitle:
+          "Pozele ne permit sa pastram vii toate momentele speciale din viata noastra. Noi suntem alaturi de tine si imortalizam nuntile, aniversarile, botezele si toate celelalte momente care merita pastrate cum ar fi maternitatea, craciunul etc.",
+        ctaText: "Hai sa vorbim",
+        ctaURL: "/contact",
+        images: [
+          "/images/wedding1.webp",
+          "/images/wedding2.webp",
+          "/images/wedding3.webp",
+        ],
+      },
+    },
   },
   crisp: {
     // Crisp website ID. IF YOU DON'T USE CRISP: just remove this => Then add a support email in this config file (mailgun.supportEmail) otherwise customer support won't work.
